40891f8b2bbab49fad01e17a659880b41fd040ee
yunlong.syl <yunlong.syl@dtwave-inc.com>
2019-01-24 17:39:36 +0800

    update: component/hello

--- components/hello/hello.js
+++ components/hello/hello.js
@@ -4,20 +4,26 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    outerNum: {
+      type: Number,
+      value: 5,
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    innerNum: 666
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    _myClick(e) {
+      const myEventDetail = {} // detail对象，提供给事件监听函数
+      this.triggerEvent('tap', myEventDetail)
+    }
   }
 })
