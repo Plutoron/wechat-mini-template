40daa4d7119744aa037c5212adb6a77fac05d9c0
yunlong.syl <yunlong.syl@dtwave-inc.com>
2019-02-06 16:53:12 +0800

    add：add-todo-input & add-todo-list

--- pages/todo-list/todo-list.js
+++ pages/todo-list/todo-list.js
@@ -4,7 +4,97 @@ import io from 'io.js'
 
 Page({
   data: {
+    list: [{ text: 'Learning Javascript', completed: true },
+      { text: 'Learning ES2016', completed: true },
+      { text: 'Learning 小程序', completed: false }],
+    todoList: [],
+    doneList: [],
+    curText: '',
+  },
+  onLoad(options) {
+    this.splitList()
+  },
+  addTodo(e) {
+    const {
+      todo,
+    } = e.detail
 
+    const {
+      list,
+     } = this.data
+
+    if (!todo) {
+      wx.showToast({
+        title: '不能添加空内容呦QAQ',
+        icon: 'none',
+      })
+      return
+    }
+
+    if (list.some(v => v.text === todo)) {
+      wx.showToast({
+        title: '已经在列表里面喽QAQ',
+        icon: 'none',
+      })
+      return
+    }
+
+    list.unshift({
+      text: todo,
+      completed: false,
+    })
+
+    this.setData({
+      list,
+    }, () => {
+      this.splitList()
+    })
+  },
+  bindstatus(e) {
+    let {
+      list,
+    } = this.data
+
+    const {
+      text,
+    } = e.currentTarget.dataset
+
+    list = list.map(v => ({
+      ...v,
+      completed: text === v.text ? !v.completed : v.completed, 
+    }))
+
+    this.setData({
+      list,
+    }, () => {
+      this.splitList()
+    })
+  },
+  binddel(e) {
+    let {
+      list,
+    } = this.data
+
+    const {
+      text,
+    } = e.currentTarget.dataset
+
+    list.some((v, i) => {
+      if (text === v.text) list.splice(i, 1)
+      return text === v.text
+    })
+
+    this.setData({
+      list,
+    }, () => {
+      this.splitList()
+    })
+  },
+  splitList() {
+    this.setData({
+      todoList: this.data.list.filter(v => !v.completed),
+      doneList: this.data.list.filter(v => v.completed),
+    })
   },
   async search() {
     try {
@@ -19,28 +109,36 @@ Page({
       })
     }
   },
-  onLoad(options) {
-    
+  bindtouchstart(e) {
+    const {
+      clientX,
+    } = e.touches[0]
+    this.setData({
+      startX: clientX,
+    })
   },
-  onReady() {
+  bindtouchmove(e) {
+    const {
+      clientX,
+    } = e.touches[0]
 
+    const {
+      text,
+    } = e.currentTarget.dataset
+
+    if (this.data.startX - clientX > 50) {
+      this.setData({
+        curText: text,
+      })
+    } else if (clientX - this.data.startX  > 50) {
+      this.setData({
+        curText: '',
+      })
+    }
   },
-  onShow() {
-
-  },
-  onHide() {
-
-  },
-  onUnload() {
-
-  },
-  onPullDownRefresh() {
-
-  },
-  onReachBottom() {
-
-  },
-  onShareAppMessage() {
-
+  bindpage(e) {
+    this.setData({
+      curText: '',
+    })
   }
 })
