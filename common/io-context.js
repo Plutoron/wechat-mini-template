ee604d45147fd0c6a2d0daa4ce0c625688c81f8b
yunlong.syl <yunlong.syl@dtwave-inc.com>
2019-03-08 21:51:27 +0800

    io-context 添加restful

--- common/io-context.js
+++ common/io-context.js
@@ -13,10 +13,29 @@ const ioContext = async ({
   dataType = 'json',
   data = {}
 }) => {
+
+  // restful 接口  参数 ':xxx' 的形式传入data
+  let isRestful 
+
+	const dataKeys = Object.keys(data)
+	const restParam = []
+
+	if (dataKeys.length > 0) {
+		if (dataKeys.some(v => v.indexOf(':') > -1)) {
+			isRestful = true
+			dataKeys.map((v, i) => {
+				if (v.indexOf(':') > -1) {
+					restParam.push(data[v])
+					delete data[v]
+				}
+			})
+		}
+	}
+
   // 所有的请求，header默认携带token
   const res = await new Promise((resolve, reject) => {
     wx.request({
-      url: `${https ? 'https' : 'http'}://${prefix[mode]}/${url}`,
+      url: `${https ? 'https' : 'http'}://${prefix[mode]}/${url}${isRestful ? '/' + restParam.join('/') : ''}`,
       header: {
         "Content-Type": "application/json",
         token: wx.getStorageSync('token') || '',
