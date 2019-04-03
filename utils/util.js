f55d49d3540f1767dfb3df70ef647779d2037f4b
yunlong.syl <yunlong.syl@dtwave-inc.com>
2019-04-03 10:27:31 +0800

    getTimestamp,   getRandomString,   getSuffix,

--- utils/util.js
+++ utils/util.js
@@ -14,7 +14,38 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+const getTimestamp = () => {
+	const date = new Date()
+	const year = date.getFullYear()
+	const month = date.getMonth() + 1 < 10
+	const day = date.getDate()
+
+	return `${year}${month < 10 ? '0' + month : month}${day < 10 ? '0' + day : day}`
+}
+
+const getRandomString = (len = 32) => {
+  const chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678'
+  const maxPos = chars.length
+  let pwd = ''
+　for (let i = 0; i < len; i++) {
+		pwd += chars.charAt(Math.floor(Math.random() * maxPos))
+	}
+	return pwd
+}
+
+const getSuffix = filename => {
+	const pos = filename.lastIndexOf('.')
+	let suffix = ''
+	if (pos != -1) {
+		suffix = filename.substring(pos)
+	}
+	return suffix
+}
+
 export {
   formatTime,
-  formatNumber
+  formatNumber,
+  getTimestamp,
+  getRandomString,
+  getSuffix,
 }
