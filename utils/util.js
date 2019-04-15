185feac2e04a2ef85c70f8f891391ec76f94a00b
suyunlongsy <suyunlongsy@gmail.com>
2019-04-15 10:04:19 +0800

    Update util.js

--- utils/util.js
+++ utils/util.js
@@ -42,10 +42,13 @@ const getSuffix = filename => {
 	return suffix
 }
 
+const checkPhone = phone => new RegExp(/^[1][3,4,5,6,7,8,9][0-9]{9}$/).test(phone)
+
 export {
   formatTime,
   formatNumber,
   getTimestamp,
   getRandomString,
   getSuffix,
+	checkPhone,
 }
