0996e4c595eb4ca93c9dd0198c04c52fdd7e3fab
yunlong.syl <yunlong.syl@dtwave-inc.com>
2019-05-06 18:03:44 +0800

    更新 io restful参数名

--- common/io-context.js
+++ common/io-context.js
@@ -11,18 +11,19 @@ const ioContext = async ({
   header = {},
   method = 'GET',
   dataType = 'json',
-  data = {}
+  data = {},
+  originRes = false,
 }) => {
 
   // restful 接口  参数 ':xxx' 的形式传入data
-  let isRestful 
+  let restful 
 
 	const dataKeys = Object.keys(data)
 	const restParam = []
 
 	if (dataKeys.length > 0) {
 		if (dataKeys.some(v => v.indexOf(':') > -1)) {
-			isRestful = true
+			restful = true
 			dataKeys.map((v, i) => {
 				if (v.indexOf(':') > -1) {
 					restParam.push(data[v])
@@ -35,7 +36,7 @@ const ioContext = async ({
   // 所有的请求，header默认携带token
   const res = await new Promise((resolve, reject) => {
     wx.request({
-      url: `${https ? 'https' : 'http'}://${prefix[mode]}/${url}${isRestful ? '/' + restParam.join('/') : ''}`,
+      url: `${https ? 'https' : 'http'}://${prefix[mode]}/${url}${restful ? '/' + restParam.join('/') : ''}`,
       header: {
         "Content-Type": "application/json",
         token: wx.getStorageSync('token') || '',
@@ -46,6 +47,17 @@ const ioContext = async ({
       dataType,
       success: (res) => {
         resolve(res.data)
+        
+        // 在这里进行统一的 返回处理
+        // if (res.data.success) {
+				// 	if (originRes) {
+				// 		resolve(res)
+				// 	} else {
+				// 		resolve(res.data.content)
+				// 	}
+				// } else {
+				// 	reject(res.data.message)
+				// }
       },
       fail: (err) => {
         reject(err)
